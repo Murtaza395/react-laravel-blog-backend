cb23af820866f4e50d5380c18a6fa361c9ad1279
Murtaza395 <mughalmurtaza999@gmail.com>
2024-08-22 15:13:59 +0500

    backend

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\TempImageController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route::post('blogs',[BlogController::class,'store']);
+Route::post('save-temp-image',[TempImageController::class,'store']);
